09d7a3b39dadd0bcc582d1023a06c86df631f6d6
fymichael <andrianiainafymichael@gmail.com>
2023-07-10 16:58:58 +0300

    Update Completion_model.php

--- application/models/Completion_model.php
+++ application/models/Completion_model.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Completion_model extends CI_Model
 {
+
+    public function getWeight($lastWeight, $weight){
+        return $weight-$lastWeight;
+    }
     public function addCompletion($id_user,$taille, $poids, $idObjectif, $objectif){
         $req = "insert into details_user values (null, %d, %d, %d, %d, %d)";
         $req = sprintf($req, $id_user, $taille, $poids, $idObjectif, $objectif);
